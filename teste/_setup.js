a40ea1bcb317e584df90d131112b115f6d91066c
Anderson Reis Souto <andersonpensephp@hotmail.com>
2017-07-14 15:29:54 -0300

    Teste include navigator in jsdom

--- teste/_setup.js
+++ teste/_setup.js
@@ -2,4 +2,5 @@ import jsdom from 'jsdom';
 const { JSDOM } = jsdom;
 const dom = new JSDOM('<html><body><p>Hello world</p></body></html>', {});
 global.window = dom.window;
+global.navigator = window.navigator;
 global.$ = require('jquery');
--- teste/teste1.js
+++ teste/teste1.js
@@ -3,12 +3,17 @@ import chai from 'chai';
 const expect = chai.expect;
 import incrementer  from '../app';
 
-describe('Suites', () => {
-    it('TESTE', () => {
+describe('Suites test', () => {
+    it('Teste de classes', () => {
         $('body').append('<div class="teste"></div>');
+        const SP = SP || {};
+        SP.insertClass = () => {
+            $('.teste').addClass('SP');
+        };
         incrementer('.teste');
+        SP.insertClass();
         expect($('body')).not.to.be.null;
-        expect($('.teste').hasClass('abc')).eql(true);
+        expect($('.teste').hasClass('abc') && $('.teste').hasClass('SP')).eql(true);
         expect($('p').text()).eql('Hello world');
     });
 });
